d5a3a2bcbbbee0efdee7614da31d0b05be08e471
YangChen <761350477@qq.com>
2018-03-05 17:22:33 +0800

    1、整理部分JavaIO的代码;

--- src/main/java/com/yc/io/InputExample.java
+++ src/main/java/com/yc/io/InputExample.java
@@ -1,45 +1,59 @@
 package com.yc.io;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 
 /**
  * Created by Administrator on 2017/7/7 0007.
- * 输入流的三种方式
+ * 输入流的四种方式
  */
 public class InputExample {
-    /*public static void main(String[] args) throws IOException {
-        File file = new File("C:\\Users\\Administrator\\Desktop\\ceshi.txt");
-        FileInputStream fileInput = new FileInputStream(file);
-        int i=0;
-        byte[] bytes = new byte[1024];
-        String data="";
 
-        while ((i = fileInput.read(bytes)) != -1){
-            data = new String(bytes,0,i);
+    public static void main(String[] args) throws IOException {
+//        method1();
+        method2();
+//        method3();
+//        method4();
+    }
+
+    /**
+     * 问题:
+     *  ->结尾处出现乱码
+     *
+     * @author YC
+     * @create 2018/3/5 17:21
+     */
+    public static void method1() throws IOException {
+        File file = new File("C:\\Users\\YC\\Desktop\\测试IO1.txt");
+        FileInputStream fileInput = new FileInputStream(file);
+        int i = 0;
+        byte[] bytes = new byte[1024];
+        String data = "";
+
+        while ((i = fileInput.read(bytes)) != -1) {
+            data = new String(bytes, 0, i);
         }
         System.out.println(data);
-    }*/
+    }
 
     /**
      * 通过FileInputStream的read(byte[] b)方法来输出显示，不乱码！
      * 关键点：把输入流信息存进byte[]数组之后，通过string s=new String(b),转换成字符串形式，然后输出！
-     * （注意：如果txt文件编码格式不出utf-8，依然会出现乱码！）
-     * @param args
-     * @throws IOException
+     * （注意：如果txt文件编码格式不是utf-8，依然会出现乱码！）
+     *
+     * @author YC
+     * @create 2018/3/5 17:09
      */
-    public static void main(String[] args) throws IOException {
+    public static void method2() {
         FileInputStream is = null;
         try {
-            is = new FileInputStream("C:\\Users\\Administrator\\Desktop\\ceui.txt");
+            is = new FileInputStream("C:\\Users\\YC\\Desktop\\测试IO1.txt");
             byte[] b = new byte[1024];
-            int i=0;
-            while((i=is.read(b))!=-1){
+            int i = 0;
+            while ((i = is.read(b)) != -1) {
                 String s = new String(b);
                 System.out.println(s);
             }
-        } catch (FileNotFoundException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         } finally {
             try {
@@ -55,21 +69,24 @@ public class InputExample {
      * 可以通过InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream,"UTF-8");的方法来解决乱码问题，如果不使用则乱码！
      * 通过While(i != -1)判断输出的结果，会在信息头部有空格；如果是通过while(inputStreamReader.read())判断则不会出现空格，原因是i是从0开始判断的，第一次输出字符为空格！
      * （注意：如果txt文件编码格式不出utf-8，依然会出现乱码！）
+     *
+     * @author YC
+     * @create 2018/3/5 17:12
      */
-    /*public static void main(String[] args) throws IOException {
-        File file = new File("C:\\Users\\Administrator\\Desktop\\ceui.txt");
+    public static void method3() throws IOException {
+        File file = new File("C:\\Users\\YC\\Desktop\\测试IO1.txt");
         FileInputStream fileInputStream = new FileInputStream(file);
-        InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream,"UTF-8");
+        InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream, "UTF-8");
         int i = 0;
 
         while (i != -1) {
             System.out.print((char) i);
             i = inputStreamReader.read();
         }
-    }*/
+    }
 
-    /*public static void main(String[] args) throws FileNotFoundException {
-        FileReader reader = new FileReader("C:\\Users\\Administrator\\Desktop\\ceshi.txt");
+    public static void method4() throws FileNotFoundException {
+        FileReader reader = new FileReader("C:\\Users\\YC\\Desktop\\测试IO1.txt");
         int i = 0;
         try {
             while (i != -1) {
@@ -78,14 +95,14 @@ public class InputExample {
             }
         } catch (IOException e) {
             e.printStackTrace();
-        }finally {
+        } finally {
             try {
-                if(reader!=null)
+                if (reader != null)
                     reader.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
-    }*/
+    }
 
 }
